1f9853a7be0fc5277408719801d5872fde182189
atiqahst <atiqah@stadvisory.com>
2024-08-02 05:28:46 +0800

    added PWA standards file

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'offline-cache';
+const OFFLINE_URL = 'index.html'; // The file to display when offline
+const CACHE_ASSETS = [
+  '/',
+  'index.html',
+  'styles.css', 
+  'questions.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
