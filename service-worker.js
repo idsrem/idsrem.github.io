398552c539f22129dc27b714f078a276e40b575f
Meezan_2108 <hamizanalimarjafri2108@gmail.com>
2025-05-19 22:28:35 +0800

    3.6.5 Update on the cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Use a versioned cache name to ensure caches are updated with each new version
-const CACHE_NAME = 'offline-cache-v3.6.4';  // Change the version number whenever there's an update
+const CACHE_NAME = 'offline-cache-v3.6.5';  // Change the version number whenever there's an update
 const OFFLINE_URL = 'index.html';      // The file to display when offline
 const CACHE_ASSETS = [
   '/',
@@ -13,7 +13,6 @@ const CACHE_ASSETS = [
   'fasa2/index.html',
   'fasa2/styles2.css',
   'fasa2/questions2.js',
-
 
   'fasa2(tutup)/index.html',
   'fasa2(tutup)/styles2.css',
