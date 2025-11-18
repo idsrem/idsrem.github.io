8f6182ea544bcfa0bfbb4136cb8e155e9c5e73a1
Meezan_2108 <hamizanalimarjafri2108@gmail.com>
2025-11-19 02:21:44 +0800

    4.2.9(5) - Add Addtional Filtered Parties on Specific Duns Final

--- cycle4/survey.js
+++ cycle4/survey.js
@@ -453,11 +453,11 @@ const dunToRelevantParties = {
   "N32 Klias": ["WARISAN", "PKS", "BEBAS - Edwin Louis", "PN", "PPRS", "KDM", "PIS", "GRS", "BN", "Tidak Pasti"],
   "N4 Tanjung Kapor": ["WARISAN", "BN", "BEBAS - Abdul Rahim Madtaip", "BEBAS - Datuk Verson Bahanda", "ANAK NEGERI", "PBK", "PIS", "SPP", "GRS", "PN", "STAR", "Tidak Pasti"],
   "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - Mohd Izahan Abdullah", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
-  "N3 Pitas": ["WARISAN", "BEBAS - Mohammad Noor Nazri Santong", "PIS", "GRS", "KDM", "BN", "BEBAS - Mohd Izahan Abdullah", "BN", "Tidak Pasti"],
-//   "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - Mohd Izahan Abdullah", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
-//   "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - Mohd Izahan Abdullah", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
-//   "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - Mohd Izahan Abdullah", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
-//   "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - Mohd Izahan Abdullah", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
+  "N3 Pitas": ["WARISAN", "BEBAS - Mohammad Noor Nazri Santong", "PIS", "GRS", "KDM", "BN", "Tidak Pasti"],
+  "N5 Matunggong": ["WARISAN", "PKS", "BEBAS - Ainin Ekon", "KDM", "PIS", "GRS", "Tidak Pasti"],
+  "N33 Kuala Penyu": ["WARISAN", "PIS", "BN", "STAR", "GRS", "PKS", "Tidak Pasti"],
+  "N15 Kiulu": ["WARISAN", "KDM", "PKS", "ANAK NEGERI", "GRS", "UPKO", "PIS", "STAR", "Tidak Pasti"],
+
 };
 
 if (question.id === "cenderungUntukUndi") {
