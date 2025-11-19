08f39182eb14c9f446936591a0a2397058d4e22a
Meezan_2108 <hamizanalimarjafri2108@gmail.com>
2025-11-19 15:05:53 +0800

    4.3.0 - Chnage the BEBAS name in N2 Bengkoka

--- cycle4/person-in-charge/survey.js
+++ cycle4/person-in-charge/survey.js
@@ -452,7 +452,7 @@ const dunToRelevantParties = {
   "N40 Bingkor": ["WARISAN", "GAS", "UPKO", "GRS", "PKS", "PIS", "KDM", "BEBAS - Ahamin Salim", "STAR", "SPP", "Tidak Pasti"],
   "N32 Klias": ["WARISAN", "PKS", "BEBAS - Edwin Louis", "PN", "PPRS", "KDM", "PIS", "GRS", "BN", "Tidak Pasti"],
   "N4 Tanjong Kapor": ["WARISAN", "BN", "BEBAS - Abdul Rahim Madtaip", "BEBAS - Datuk Verdon Bahanda", "ANAK NEGERI", "PBK", "PIS", "SPP", "GRS", "PN", "STAR", "Tidak Pasti"],
-  "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - Mohd Izahan Abdullah", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
+  "N2 Bengkoka": ["WARISAN", "GRS", "STAR", "PIS", "BEBAS - KPD Oding", "BN", "UPKO", "PN", "KDM", "PBK", "BEBAS - Jomin @ Munir Mogompit @ Magempit", "Tidak Pasti"],
   "N3 Pitas": ["WARISAN", "BEBAS - Mohammad Noor Nazri Santong", "PIS", "GRS", "KDM", "BN", "Tidak Pasti"],
   "N5 Matunggong": ["WARISAN", "PKS", "BEBAS - Ainin Ekon", "KDM", "PIS", "GRS", "Tidak Pasti"],
   "N33 Kuala Penyu": ["WARISAN", "PIS", "BN", "STAR", "GRS", "PKS", "Tidak Pasti"],
